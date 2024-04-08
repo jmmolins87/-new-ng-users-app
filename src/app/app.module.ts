240e7d5c2c9c73bb91995c54d84d417557e9219c
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-04-08 16:28:08 +0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent,
+    ListUsersComponent
   ],
   imports: [
     BrowserModule,
